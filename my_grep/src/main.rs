174993e096f2668de3c15d0a1f17529ab62c4361
Salavat-VR <shashkevich41@gmail.com>
2021-10-26 21:15:10 +0300

    implemented & refactored all the GREP proj

--- my_grep/src/main.rs
+++ my_grep/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use my_grep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = my_grep::run(config) {
+        eprintln!("Application error: {}", err);
+        process::exit(1);
+    }
 }
