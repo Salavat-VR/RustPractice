94c00f12e6834c02c471c527aaa154e8d1be0e13
Salavat-VR <shashkevich41@gmail.com>
2021-11-26 21:06:19 +0200

    stack implementation in Rust

--- stack and queue/src/main.rs
+++ stack and queue/src/main.rs
@@ -1,14 +1,5 @@
 #[allow(dead_code)]
-fn main() {
-    let mut my_stack = Stack { stack: Vec::new()};
-    my_stack.push(5);
-    my_stack.push(2);
-    my_stack.push(10);
-    my_stack.push(6);
-
-    my_stack.pop();
-    assert_eq!(my_stack.peek().unwrap(), &10);
-}
+fn main() {}
 
 struct Stack<T> {
     stack: Vec<T>,
@@ -37,5 +28,63 @@ impl<T> Stack<T> {
 
     fn peek(&self) -> Option<&T> {
         self.stack.last()
+    }
+}
+
+struct Queue<T> {
+    queue: Vec<T>,
+}
+
+impl<T> Queue<T> {
+    fn new() -> Queue<T> {
+        Queue { queue: Vec::new() }
+    }
+
+    fn enqueue(&mut self, item: T) {
+        self.queue.push(item)
+    }
+
+    fn dequeue(&mut self) -> T {
+        self.queue.remove(0)
+    }
+
+    fn length(&self) -> usize {
+        self.queue.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.queue.is_empty()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.queue.first()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_stack() {
+        let mut my_stack = Stack { stack: Vec::new() };
+        my_stack.push(5);
+        my_stack.push(2);
+        my_stack.push(10);
+        my_stack.push(6);
+
+        my_stack.pop();
+        assert_eq!(my_stack.peek().unwrap(), &10);
+    }
+
+    fn test_queue() {
+        let mut my_queue = Queue { queue: Vec::new() };
+
+        my_queue.enqueue(1);
+        my_queue.enqueue(2);
+        my_queue.enqueue(4);
+        my_queue.dequeue();
+
+        assert_eq!(my_queue.peek().unwrap(), &2);
     }
 }
