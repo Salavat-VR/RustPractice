787d39858d74407b7770c87b7f90ae3e1624c9d9
Salavat-VR <shashkevich41@gmail.com>
2021-11-26 21:06:51 +0200

    queue implementation in Rust

---
+++ stack and queue/target/debug/deps/stack-1d5696c1ee2dcaa3.d
@@ -0,0 +1,5 @@
+C:\Users\User\Desktop\wks\RustPractice\stack and queue\target\debug\deps\stack-1d5696c1ee2dcaa3.exe: src\main.rs
+
+C:\Users\User\Desktop\wks\RustPractice\stack and queue\target\debug\deps\stack-1d5696c1ee2dcaa3.d: src\main.rs
+
+src\main.rs:
