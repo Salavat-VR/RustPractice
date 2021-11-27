15b186b5f5ab047c924602bf10ff97e6b8833336
Salavat-VR <shashkevich41@gmail.com>
2021-11-27 19:51:41 +0200

    valid parentheses riddle using stack implementation

---
+++ valid_parentheses/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {}
+
+fn is_valid(s: String) -> bool {
+    let mut stack: Vec<char> = vec![];
+    for c in s.chars() {
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' | ']' | '}' => match stack.pop() {
+                Some(t) => {
+                    if !((t == '{' && c == '}') || (t == '(' && c == ')') || (t == '[' && c == ']'))
+                    {
+                        return false;
+                    }
+                }
+                None => {
+                    return false;
+                }
+            },
+            _ => {}
+        }
+    }
+    stack.is_empty()
+}
+
+#[test]
+fn test() {
+    assert_eq!(is_valid(String::from("()")), true);
+    assert_eq!(is_valid(String::from("()[]{}")), true);
+    assert_eq!(is_valid(String::from("(]")), false);
+    assert_eq!(is_valid(String::from("([)]")), false);
+    assert_eq!(is_valid(String::from("{[]}")), true);
+}
