8e5f8227d04e9fa1832a0da41f8233a6b879fb7c
Salavat-VR <shashkevich41@gmail.com>
2021-11-28 22:48:13 +0200

    n th prime number using Sieve of Eratosthenes approach

---
+++ n_th_prime_number/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    assert_eq!(nth(5), 13);
+}
+
+pub fn nth(n: u32) -> u32 {
+    let mut p = 3;
+    let mut primes = vec![2];
+    while n >= primes.len() as u32 {
+        if !primes.iter().any(|pn| p % pn == 0) {
+            primes.push(p);
+        }
+        p += 1;
+    }
+    primes.pop().unwrap()
+}
